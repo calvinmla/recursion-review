b0c635905699dec870dd73334935f0156ad7d39e
Calvin <calvinminhla@gmail.com>
2020-10-15 15:45:30 -0700

    Made all tests pass for stringifiable objects in stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  // array / object
+  if (Array.isArray(obj)) {
+
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var results = '[';
+    for (var i = 0; i < obj.length; i++) {
+      var result = stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        results += result + ',';
+      } else {
+        results += result;
+      }
+    }
+    results += ']';
+    return results;
+  } else if (typeof obj === 'object' && obj !== null && obj !== undefined) {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    var results = '{';
+    for (var keys in obj) {
+      var keyString = stringifyJSON(keys);
+      var valueString = stringifyJSON(obj[keys]);
+      if (Object.keys(obj)[Object.keys(obj).length - 1] !== keys) {
+        results += keyString + ':' + valueString + ',';
+      } else {
+        results += keyString + ':' + valueString;
+      }
+    }
+    results += '}';
+    return results;
+  }
+
+  // string
+  if (typeof obj === 'string') {
+    console.log('"' + obj + '"');
+    return '"' + obj + '"';
+  } else {
+    return obj + '';
+  }
+
+
 };
